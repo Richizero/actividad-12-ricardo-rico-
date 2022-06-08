5c7e80811ad43975db905e6f05dc9df55759dc97
Richizero <richizero@hotmail.com>
2022-06-07 20:54:26 -0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading]= useState(true)
+  const [data, setData]= useState([])
+
+  const getMovies = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5");//tarea 1
+    const json = await response.json();//tarea 2
+    setData(json);
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <>
+            <Text>{item.title}</Text>
+            <Image source={{uri: item.url}}
+            style={{width: 150, height: 150}} />
+            </>
+          )}
+        />
+      
     </View>
   );
 }
